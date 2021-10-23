855a0e2595ad23cce18f38829ce5f87e71e65e4d
Victor Jordan <victorjordan95@gmail.com>
2021-10-23 10:49:47 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>inicios | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>world</span>
+      </h1>
+    </>
+
+  );
+}
