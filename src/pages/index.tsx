6ae69ac19b7c9e11adc3fb38c7662e7cc5a36f54
Victor Jordan <victorjordan@fitec.org.br>
2021-10-26 15:30:40 -0300

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>inicios | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>world</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
 
   );
